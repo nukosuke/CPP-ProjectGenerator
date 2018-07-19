671dc11a4f06be3b4d954002b5b99709b1f9e2dc
nukosuke <nukosuke@lavabit.com>
2018-07-20 01:01:57 +0900

    CLI basic implementation

--- lib/CPP/ProjectGenerator/CLI.pm
+++ lib/CPP/ProjectGenerator/CLI.pm
@@ -1,13 +1,35 @@
-package CLI::ProjectGenerator::CLI;
+package CPP::ProjectGenerator::CLI;
 use strict;
-use Cwd ();
-use Getopt::Long ();
+use Cwd;
+use Getopt::Long;
+use Pod::Usage;
 
 sub new {
     my $class = shift;
     bless {
         template_path => 'template',
     }, $class;
+}
+
+sub parse_options {
+    my $self = shift;
+
+    Getopt::Long::Configure("bundling");
+    Getopt::Long::GetOptions(
+        'h|help' => sub { $self->{action} = 'show_help'; },
+    );
+}
+
+sub run {
+    my $self = shift;
+
+    if (my $action = $self->{action}) {
+        $self->$action() and return 1;
+    }
+}
+
+sub show_help {
+    pod2usage(2);
 }
 
 1;
