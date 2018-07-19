872f05f8acd7991565b0f0e78356821e899420ac
nukosuke <nukosuke@lavabit.com>
2018-07-19 22:54:28 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+CPP::ProjectGenerator - It's new $module
+
+# SYNOPSIS
+
+    use CPP::ProjectGenerator;
+
+# DESCRIPTION
+
+CPP::ProjectGenerator is ...
+
+# LICENSE
+
+Copyright (C) nukosuke.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nukosuke <nukosuke@lavabit.com>
---
+++ lib/CPP/ProjectGenerator.pm
@@ -0,0 +1,39 @@
+package CPP::ProjectGenerator;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+CPP::ProjectGenerator - It's new $module
+
+=head1 SYNOPSIS
+
+    use CPP::ProjectGenerator;
+
+=head1 DESCRIPTION
+
+CPP::ProjectGenerator is ...
+
+=head1 LICENSE
+
+Copyright (C) nukosuke.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nukosuke E<lt>nukosuke@lavabit.comE<gt>
+
+=cut
+
